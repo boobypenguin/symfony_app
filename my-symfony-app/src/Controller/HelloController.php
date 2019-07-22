7776fc77237b042d173f04c0ef5412c250858f06
boobypenguin <boobypenguin99@gmail.com>
2019-07-22 12:36:33 +0900

    commit

--- my-symfony-app/src/Controller/HelloController.php
+++ my-symfony-app/src/Controller/HelloController.php
@@ -1,10 +1,11 @@
 <?php
 namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
@@ -12,13 +13,43 @@ class HelloController extends AbstractController
     /**
      * @Route("/hello", name="hello")
      */
-    public function index(Request $request, LoggerInterface $logger)
+    public function index(Request $request, SessionInterface $session)
     {
-        $data = array(
-            'name' => array('first' => 'Taro', 'second' => 'Yamada'),
-            'age' => 36, 'mail' => 'taro@yamada.kun',
-        );
-        $logger->info(serialize($data));
-        return new JsonResponse($data);
+        $data = new MyData();
+        $form = $this->createFormBuilder($data)
+            ->add('data', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Click'])
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            $data = $form->getData();
+            if ($data->getData() == '!') {
+                $session->remove('data');
+            } else {
+                $session->set('data', $data->getData());
+            }
+        }
+
+        return $this->render('hello/index.html.twig', [
+            'title' => 'Hello',
+            'data' => $session->get('data'),
+            'form' => $form->createView(),
+        ]);
+    }
+}
+
+// データ用クラス
+class MyData
+{
+    protected $data = '';
+
+    public function getData()
+    {
+        return $this->data;
+    }
+    public function setData($data)
+    {
+        $this->data = $data;
     }
 }
